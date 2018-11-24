558ca5b3dfaa70424bdf2846170b64ea0692355e
hufangtao <hufangta@qq.com>
2018-11-24 09:20:38 +0800

    init commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <thread>
-
+#include "boost/asio.hpp"
+using namespace boost::asio;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    io_service service;
+    ip::tcp::endpoint ep(ip::address::from_string("127.0.0.1"), 2001);
+    ip::tcp::socket sock(service);
+    sock.connect(ep);
     return 0;
 }
