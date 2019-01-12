d579b89646e0d62b7652454bf1b301ed5c397d81
hufangtao <hufangta@qq.com>
2019-01-12 09:44:43 +0800

    all

--- src/utils/log/log.h
+++ src/utils/log/log.h
@@ -35,33 +35,4 @@ class log
     static std::ostream &getStream();
 };
 
-std::string printLog(){
-    return "";
-}
-
-template <class T, class... Args>
-std::string printLog(T head, Args... rest){
-    std::stringstream ss;
-    std::string temp;
-    ss<<head;
-    ss>>temp;
-    return temp + printLog(rest...);
-}
-
-template <class... T>
-void LOG(std::string type,std::string date, std::string time, std::string file, std::string func, int line, T... args)
-{
-    std::stringstream ss;
-    std::string logMsg = "";
-    ss<<"["<<type<<"]"<<date<<time<<file<<func<<line;
-    ss>>logMsg;
-    logMsg += printLog(args...);
-    log::addMsg(logMsg);
-}
-
-#define DEBUG(MSG...) LOG("DEBUG",__DATE__, __TIME__, __FILE__, __FUNCTION__, __LINE__, ##MSG)
-#define INFO(MSG...) LOG("INFO",__DATE__, __TIME__, __FILE__, __FUNCTION__, __LINE__, ##MSG)
-#define WARN(MSG...) LOG("WARN",__DATE__, __TIME__, __FILE__, __FUNCTION__, __LINE__, ##MSG)
-#define ERROR(MSG...) LOG("ERROR",__DATE__, __TIME__, __FILE__, __FUNCTION__, __LINE__, ##MSG)
-
 #endif
