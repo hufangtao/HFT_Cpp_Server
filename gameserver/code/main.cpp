74c508a4189002a0340191c436349c0531c2e8d9
fangtao.hu <fangtao.hu@mihoyo.com>
2020-01-20 19:43:46 +0800

    调整boost文件位置

--- gameserver/code/main.cpp
+++ gameserver/code/main.cpp
@@ -1,8 +1,25 @@
-#include "iostream"
+#include <iostream>
+#include <boost/lexical_cast.hpp>
+#include <boost/timer.hpp>
+using namespace boost;
 using namespace std;
 
 int main()
 {
+    timer t;
+    int a = lexical_cast<int>("12");
+    cout << a << endl;
     cout << "Hello World" << endl;
+    try
+    {
+        int err = lexical_cast<int>("789edc", 4);
+        cout << err << endl;
+    }
+    catch (const std::exception& e)
+    {
+        cout << e.what() << endl;
+    }
+    usleep(1231);
+    cout << "Test:" << t.elapsed() << endl;
     return 0;
 }
