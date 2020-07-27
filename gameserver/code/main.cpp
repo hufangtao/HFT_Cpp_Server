6d7b2fc94033763393f83c495f379997f42ac1c7
Fangtao.Hu <hufangta@qq.com>
2020-07-26 18:16:36 -0700

    asio timer

--- gameserver/code/main.cpp
+++ gameserver/code/main.cpp
@@ -3,22 +3,8 @@
 #include "player.h"
 using namespace std;
 
-const char* getStringPtr()
-{
-    string str("test");
-    cout << str << endl;
-    cout << str.c_str() << endl;
-    return str.c_str();
-}
-
 int main()
 {
-    map<int, int> test_map;
-    for (auto iter = test_map.begin(); iter != test_map.end(); ++iter)
-    {
-    }
-
-    const char* a = getStringPtr();
-    cout << *a << *(a+2) << endl;
-    return 0;
+	return 0;
 }
+
